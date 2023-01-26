7ebe0c43b5b12f1d9210cdc757f19d15f3f5d7af
Lorenzo Lezcano <lorenzodlezcano@gmail.com>
2023-01-26 16:33:14 -0300

    security warning update

--- django_password_generator/settings.py
+++ django_password_generator/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-67@r#xx$qlj)9n6^!3qxc$n$=fotcp1z&iok4b27m@h-68ry@v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
